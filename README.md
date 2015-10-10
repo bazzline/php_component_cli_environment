33519f86afaa6c65586b69072bc2b68b58abdab5
stevleibelt <stev@leibelt.de>
2015-10-10 23:25:39 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# php_component_cli_environment
-free as in freedom command line environment for php creating lean command line executable files
+# Command Line Environment for PHP
+
+This project aims to deliver an free as in freedom, easy to use and lean as in fat free environment to create executable command line php files.
+
+This project does not want to be competitive to a big console environment like the once mentioned below in the [link section](#links).
+
+# Usage
+
+## Create Executable Command Line File
+
+```
+./bin/net_bazzline_create_executable_command_line_file bin/hello
+```
+
+## Write Your Code
+
+```php
+#open bin/hello
+#replace line "$usage			= basename(__FILE__) . ' [-v|--verbose]';" with the line below
+$usage			= basename(__FILE__) . ' <your name> [-v|--verbose]';
+#replace the line "//put in your business logic" with the code below
+//begin of dependencies
+$arguments  = $environment->getArguments();
+$values     = $arguments->getValues();
+//end of dependencies
+
+//begin of argument validation
+$valuesNotAreValid = (count($values) == 0);
+
+if ($valuesNotAreValid) {
+    throw new InvalidArgumentException(
+        'invalid number of arguments provided'
+    );
+}
+//end of argument validation
+
+//begin of business logic
+$name = ucfirst($values[0]);
+
+$environment->outputIfVerbosityIsEnabled('provided values are: ' . implode(' ', $values));
+$environment->output('Hello ' . $name);
+//end of business logic
+
+#execute ./bin/hello world
+```
+
+# Example
+
+* [hello](https://github.com/bazzline/php_component_cli_environment/tree/master/example/hello)
+
+# Install
+
+## By Hand
+
+```
+mkdir -p vendor/net_bazzline/php_component_cli_environment
+cd vendor/net_bazzline/php_component_cli_environment
+git clone https://github.com/bazzline/php_component_cli_environment .
+```
+
+## With [Packagist](https://packagist.org/packages/net_bazzline/php_component_cli_environment)
+
+```
+    composer require net_bazzline/php_component_cli_environment:dev-master
+```
+
+# API
+
+[API](http://www.bazzline.net/----/index.html) is available at [bazzline.net](http://www.bazzline.net).
+
+
+# History
+
+* upcomming
+    * @todo
+        * add link to openhub
+        * write tests
+* [0.1.0](https://github.com/bazzline/php_component_cli_environment/tree/1.5.0) - released at 10.10.2015
+    * initial release
+
+# Links
+
+* [hoaproject console](https://github.com/hoaproject/Console)
+* [symfony console](https://github.com/symfony/console)
+* [windwalker console](https://github.com/ventoviro/windwalker-console)
+* [duncan3dc console](https://github.com/duncan3dc/console)
+* [webmozart console](https://github.com/webmozart/console)
+* [zend console](https://github.com/zendframework/zend-console)
---
+++ source/Net/Bazzline/Component/Cli/Environment/CommandLineEnvironment.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2015-10-10
+ */
+
+namespace Net\Bazzline\Component\Cli\Environment;
+
+use Exception;
+use InvalidArgumentException;
+use Net\Bazzline\Component\Cli\Arguments\Arguments;
+use Net\Bazzline\Component\Toolbox\Scalar\Text;
+
+class CommandLineEnvironment
+{
+    /** @var Arguments */
+	private $arguments;
+
+    /** @var bool */
+    private $beVerbose;
+
+    /** @var Callable */
+    private $executeExceptionHandler;
+
+    /** @var Text */
+	private $text;
+
+    /**
+     * @param array $argv
+     */
+	public function __construct($argv)
+	{
+		$this->arguments    = new Arguments($argv);
+        $this->text         = new Text();
+        $this->determineIfWeAreVerbose($this->arguments);
+        $this->setDefaultExecuteExceptionHandler();
+	}
+
+    /**
+     * @return bool
+     */
+    public function beVerbose()
+    {
+        return $this->beVerbose;
+    }
+
+    /**
+     * $instruction is called with argument CommandLineEnvironment
+     * \Exception thrown by $instruction are handled
+     *
+     * @param Callable $instruction
+     * @param string $usage
+     * @throws InvalidArgumentException
+     */
+	public function execute($instruction, $usage)
+	{
+		try {
+            $this->throwInvalidArgumentExceptionIfNoCallableProvided($instruction);
+			$instruction($this);
+		} catch (Exception $exception) {
+            $handler = $this->executeExceptionHandler;
+            $handler($exception, $usage);
+		}
+	}
+
+    /**
+     * @return Arguments
+     */
+	public function getArguments()
+	{
+		return $this->arguments;
+	}
+
+    /**
+     * @return Text
+     */
+	public function getText()
+	{
+		return $this->text;
+	}
+
+    /**
+     * Handler is called with arguments:
+     *  \Exception $exception
+     *  string $usage
+     *
+     * @param callable $handler
+     * @throws InvalidArgumentException
+     */
+    public function overwriteDefaultExecuteExceptionHandler($handler)
+    {
+        $this->throwInvalidArgumentExceptionIfNoCallableProvided($handler);
+        $this->executeExceptionHandler = $handler;
+    }
+
+    /**
+     * @param int|float|string|array $scalarOrArrayOfScalars
+     * @throws InvalidArgumentException
+     */
+	public function output($scalarOrArrayOfScalars)
+	{
+		if (is_scalar($scalarOrArrayOfScalars)) {
+			$scalarOrArrayOfScalars = array($scalarOrArrayOfScalars);
+		}
+
+        if (!is_array($scalarOrArrayOfScalars)) {
+            throw new InvalidArgumentException(
+                'provided argument must be scalar or array'
+            );
+        }
+
+        echo implode(PHP_EOL, $scalarOrArrayOfScalars) . PHP_EOL;
+	}
+
+    /**
+     * @param int|float|string|array $scalarOrArrayOfScalars
+     */
+	public function outputIfVerbosityIsEnabled($scalarOrArrayOfScalars)
+	{
+		if ($this->beVerbose) {
+			$this->output($scalarOrArrayOfScalars);
+		}
+	}
+
+    /**
+     * @param Arguments $arguments
+     */
+    private function determineIfWeAreVerbose(Arguments $arguments)
+    {
+        $this->beVerbose = ($arguments->hasFlag('verbose') || $arguments->hasFlag('v'));
+    }
+
+    private function setDefaultExecuteExceptionHandler()
+    {
+        $environment                    = $this;
+        $this->executeExceptionHandler  = function (Exception $exception, $usage) use ($environment) {
+			$environment->output(
+                array(
+                    'An error occurred',
+                    $exception->getMessage(),
+                    '',
+                    'Usage: ' . $usage
+			    )
+            );
+			exit(1);
+        };
+    }
+
+    /**
+     * @param Callable $callable
+     * @throws InvalidArgumentException
+     */
+    private function throwInvalidArgumentExceptionIfNoCallableProvided($callable)
+    {
+        if (!is_callable($callable)) {
+            throw new InvalidArgumentException(
+                'no callable provided'
+            );
+        }
+    }
+}
